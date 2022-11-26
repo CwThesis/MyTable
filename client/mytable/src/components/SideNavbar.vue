62c9ddb1417229192e15be2be534a43d4a1e71c5
mariarosz <roszykmaria@gmail.com>
2022-11-26 18:09:20 +0100

    feat: side navbar

---
+++ client/mytable/src/components/SideNavbar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="w-64 bg-gray-50 border-r border-gray-200">
+    <div class="py-4 px-6">
+      <a href="/"><h1>my table</h1></a>
+    </div>
+
+    <div class="py-4 px-6">
+      <div class="mb-10">
+        <a href="#" class="py-2 px-4 text-sm text-gray-700">Overview</a>
+      </div>
+      <div class="mb-10">
+        <a href="#" class="py-2 px-4 text-sm text-gray-700">Tickets</a>
+      </div>
+      <div class="mb-10">
+        <a href="#" class="flex items-center py-2 px-4 text-sm text-gray-700"
+          >Tables</a
+        >
+      </div>
+      <div class="mb-10">
+        <a href="#" class="py-2 px-4 text-sm text-gray-700">Menu</a>
+      </div>
+    </div>
+  </div>
+</template>
--- client/mytable/src/views/DashboardView.vue
+++ client/mytable/src/views/DashboardView.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import TopNavbar from "../components/TopNavbar.vue";
+import SideNavbar from "../components/SideNavbar.vue";
+import OnboardingForm from "../components/OnboardingForm.vue";
 </script>
 <template>
-  <TopNavbar></TopNavbar>
+  <div class="flex min-h-screen">
+    <SideNavbar />
+
+    <div class="flex-1">
+      <TopNavbar />
+      <main class="flex-1 justify-center">
+        <OnboardingForm />
+      </main>
+    </div>
+  </div>
 </template>
