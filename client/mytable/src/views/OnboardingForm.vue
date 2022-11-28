60bfc36b4eb7e85b6712ab5b29cb4ed2a74dbce8
mariarosz <roszykmaria@gmail.com>
2022-11-28 17:10:39 +0100

    feat: onboarding form

--- client/mytable/src/views/OnboardingForm.vue
+++ client/mytable/src/views/OnboardingForm.vue
@@ -1,137 +1,218 @@
 <script setup lang="ts">
 import TopNavbarVue from "@/components/TopNavbar.vue";
+import { ref } from "vue";
+
+const staff = ref<string[]>([]);
+
+function addWaiter(event: Event) {
+  const target = event.target as HTMLInputElement;
+  staff.value.push(target.value);
+  target.value = "";
+}
+
+function submitForm() {
+  console.log("Form submitted!");
+
+}
 </script>
 
 <template>
   <TopNavbarVue></TopNavbarVue>
-  <div>
-    <h1>Welcome to my table!</h1>
-    <h1>You're almost ready to start.</h1>
-    <h3>We'll need some more information to complete your onboarding.</h3>
-  </div>
-
   <div class="container mx-auto content-center max-w-5xl">
-    <form action="#" method="POST">
-      <div class="shadow sm:overflow-hidden sm:rounded-md">
-        <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
-          <div class="grid grid-cols-3 gap-6">
-            <div class="col-span-3 sm:col-span-2">
-              <label
-                for="company-website"
-                class="block text-sm font-medium text-gray-700"
-                >Website</label
-              >
-              <div class="mt-1 flex rounded-md shadow-sm">
-                <span
-                  class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500"
-                  >http://</span
-                >
-                <input
-                  type="text"
-                  name="company-website"
-                  id="company-website"
-                  class="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  placeholder="www.example.com"
-                />
-              </div>
-            </div>
-          </div>
+    <div class="shadow sm:overflow-hidden sm:rounded-md mb-4 mt-4">
+      <div class="space-y-4 bg-gray-100 px-4 py-5 sm:p-6">
+        <h1 class="text-2xl">Welcome to my table!</h1>
+        <h3 class="text-sm font-medium text-gray-700">
+          You're almost ready to start.
+        </h3>
+        <p class="mt-2 text-sm text-gray-500">
+          We'll need some more information to complete your onboarding.
+        </p>
+      </div>
+    </div>
 
-          <div>
-            <label for="about" class="block text-sm font-medium text-gray-700"
-              >About</label
-            >
-            <div class="mt-1">
-              <textarea
-                id="about"
-                name="about"
-                rows="3"
-                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                placeholder="you@example.com"
-              />
-            </div>
-            <p class="mt-2 text-sm text-gray-500">
-              Brief description for your profile. URLs are hyperlinked.
-            </p>
-          </div>
-
-          <div>
-            <label class="block text-sm font-medium text-gray-700">Photo</label>
-            <div class="mt-1 flex items-center">
-              <span
-                class="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100"
-              >
-                <svg
-                  class="h-full w-full text-gray-300"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                </svg>
-              </span>
-              <button
-                type="button"
-                class="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Change
-              </button>
-            </div>
-          </div>
-
-          <div>
-            <label class="block text-sm font-medium text-gray-700"
-              >Cover photo</label
-            >
-            <div
-              class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6"
-            >
-              <div class="space-y-1 text-center">
-                <svg
-                  class="mx-auto h-12 w-12 text-gray-400"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 48 48"
-                  aria-hidden="true"
-                >
-                  <path
-                    d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <div class="flex text-sm text-gray-600">
-                  <label
-                    for="file-upload"
-                    class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                  >
-                    <span>Upload a file</span>
+    <div class="mt-10 sm:mt-0">
+      <div class="md:grid md:grid-cols-6 md:gap-6">
+        <div class="mt-5 md:col-span-6 md:col-start-1 md:mt-0">
+          <form action="#" @submit.prevent="submitForm" method="POST">
+            <div class="overflow-hidden shadow sm:rounded-md">
+              <div class="bg-white px-4 py-5 sm:p-6">
+                <div class="grid grid-cols-6 gap-6">
+                  <div class="col-span-6 sm:col-span-4">
+                    <label
+                      for="email-address"
+                      class="block text-sm font-medium text-gray-700"
+                      >Restaurant Name</label
+                    >
                     <input
-                      id="file-upload"
-                      name="file-upload"
-                      type="file"
-                      class="sr-only"
+                      type="text"
+                      name="email-address"
+                      id="email-address"
+                      autocomplete="email"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     />
-                  </label>
-                  <p class="pl-1">or drag and drop</p>
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="first-name"
+                      class="block text-sm font-medium text-gray-700"
+                      >Bank Name</label
+                    >
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autocomplete="given-name"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="last-name"
+                      class="block text-sm font-medium text-gray-700"
+                      >IBAN</label
+                    >
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autocomplete="family-name"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6">
+                    <label
+                      for="street-address"
+                      class="block text-sm font-medium text-gray-700"
+                      >Street address</label
+                    >
+                    <input
+                      type="text"
+                      name="street-address"
+                      id="street-address"
+                      autocomplete="street-address"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                    <label
+                      for="city"
+                      class="block text-sm font-medium text-gray-700"
+                      >City</label
+                    >
+                    <input
+                      type="text"
+                      name="city"
+                      id="city"
+                      autocomplete="address-level2"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label
+                      for="postal-code"
+                      class="block text-sm font-medium text-gray-700"
+                      >ZIP / Postal code</label
+                    >
+                    <input
+                      type="text"
+                      name="postal-code"
+                      id="postal-code"
+                      autocomplete="postal-code"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label
+                      for="country"
+                      class="block text-sm font-medium text-gray-700"
+                      >Country</label
+                    >
+                    <select
+                      id="country"
+                      name="country"
+                      autocomplete="country-name"
+                      class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    >
+                      <option>United States</option>
+                      <option>Canada</option>
+                      <option>Mexico</option>
+                    </select>
+                  </div>
+
+                  <div class="hidden sm:block col-span-6" aria-hidden="true">
+                    <div class="py-5">
+                      <div class="border-t border-gray-200" />
+                    </div>
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="country"
+                      class="block text-sm font-medium text-gray-700"
+                      >How many tables do you have?</label
+                    >
+                    <select
+                      id="table-number"
+                      name="table-number"
+                      class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    >
+                      <option v-for="num in 20" :key="num">{{ num }}</option>
+                    </select>
+                    <p class="mt-2 text-sm text-gray-500">
+                      We'll preconfigure your QR codes.
+                    </p>
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="waiter-name"
+                      class="block text-sm font-medium text-gray-700"
+                      >Your staff</label
+                    >
+                    <input
+                      @keyup.enter="addWaiter"
+                      type="text"
+                      name="waiter-name"
+                      id="waiter-name"
+                      autocomplete="waiter-name"
+                      class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                    <p class="mt-2 text-sm text-gray-500">
+                      Add your waiters' names here. You will be able to edit
+                      this later.
+                    </p>
+                    <div class="flex flex-row flex-wrap">
+                      <li
+                        class="mt-2 text-sm text-gray-500 list-none bg-gray-100 rounded-md m-1 py-1 px-1"
+                        v-for="item in staff"
+                        :key="item"
+                      >
+                        {{ item }}
+                      </li>
+                    </div>
+                  </div>
                 </div>
-                <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+              </div>
+
+              <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                <button
+                  type="submit"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Save
+                </button>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-          <button
-            type="submit"
-            class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            Save
-          </button>
+          </form>
         </div>
       </div>
-    </form>
+    </div>
   </div>
 </template>
