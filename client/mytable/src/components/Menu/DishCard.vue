1d15e220ecdfa212efbf21e66b49e31764bfe9d9
René Beltrán <renebeltranromero@gmail.com>
2022-11-28 17:18:03 +0100

    FE tables, tickets, menu 40%

---
+++ client/mytable/src/components/Menu/DishCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps({
+  dish: Object,
+});
+</script>
+
+<template>
+  <li
+    class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move border border-white"
+  >
+    <div class="flex items-center">
+      <img
+        class="w-20 h-20 rounded-full"
+        :src="props.dish.img"
+        :alt="props.dish.title"
+      />
+    </div>
+    <p class="ml-5 text-gray-700 font-semibold font-sans tracking-wide">
+      {{ props.dish.title }}
+    </p>
+    <div class="flex items-center">
+      <p class="ml-8 text-gray-700 font-sans tracking-wide">
+        {{ props.dish.description }}
+      </p>
+    </div>
+    <div class="flex"></div>
+  </li>
+</template>
---
+++ client/mytable/src/components/Tickets/TicketsView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import SideNavbar from "../SideNavbar.vue";
+import TopNavbar from "../TopNavbar.vue";
+import TicketsGrid from "./TicketsGrid.vue";
+
+
+</script>
+
+<template>
+  <div class="flex min-h-screen">
+    <SideNavbar />
+    <div class="flex-1">
+      <TopNavbar />
+      <main class="flex-1 justify-center">
+        <TicketsGrid></TicketsGrid>
+      </main>
+    </div>
+  </div>
+</template>
