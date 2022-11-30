d71c5ecc3da02c460a5ab1cc99331ea4241a49d0
mariarosz <roszykmaria@gmail.com>
2022-11-30 19:49:52 +0100

    feat: add and remove dishes from order

--- client/vue-project/src/views/MobileView.vue
+++ client/vue-project/src/views/MobileView.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
-import type { Dish } from "../types";
+import type { Dish, DishIdWithAmount } from "../types";
 import { computed } from "vue";
-import DishCard from "../components/Menu/DishCard.vue";
+import DishCardMobile from "../components/Mobile/DishCardMobile.vue";
+import type { Ref } from 'vue';
+import { ref, toRaw } from "vue";
 
+
+let order: Ref<DishIdWithAmount[]> = ref([]);
+
+// fetching dishes ---->
 const dishes: Dish[] = [
   {
     img: "https://images.hola.com/imagenes/cocina/recetas/20200917175530/paella-valenciana-clasica/0-866-670/paella-age-t.jpg",
@@ -11,6 +17,7 @@ const dishes: Dish[] = [
       "Typical spanish paella, contains rice, chicken, seafood and vegetables",
     price: "21€",
     category: "mains",
+    id: 1
   },
   {
     img: "https://www.laylita.com/recetas/wp-content/uploads/2021/02/Receta-de-la-arepa-reina-pepiada.jpg",
@@ -18,6 +25,7 @@ const dishes: Dish[] = [
     description: "Arepa with cheese, ham and egg",
     price: "9€",
     category: "starters",
+    id: 2
   },
   {
     img: "https://www.laylita.com/recetas/wp-content/uploads/2021/02/Receta-de-la-arepa-reina-pepiada.jpg",
@@ -25,6 +33,7 @@ const dishes: Dish[] = [
     description: "Chocolate and raspberries with nuts",
     price: "9€",
     category: "desserts",
+    id: 3
   },
   {
     img: "https://images.hola.com/imagenes/cocina/recetas/20200917175530/paella-valenciana-clasica/0-866-670/paella-age-t.jpg",
@@ -33,6 +42,7 @@ const dishes: Dish[] = [
       "Typical spanish paella, contains rice, chicken, seafood and vegetables",
     price: "21€",
     category: "mains",
+    id: 4
   },
   {
     img: "https://images.hola.com/imagenes/cocina/recetas/20200917175530/paella-valenciana-clasica/0-866-670/paella-age-t.jpg",
@@ -40,8 +50,10 @@ const dishes: Dish[] = [
     description: "Fresco y platano",
     price: "4€",
     category: "drinks",
+    id: 5
   },
 ];
+
 
 const sortByCategory = computed(() => {
   const menu = {
@@ -54,6 +66,35 @@ const sortByCategory = computed(() => {
 
   return menu;
 });
+
+function addToOrder (dish: Dish) {
+   if (order.value.find(element => element.id === dish.id)) {
+   const object = order.value.find(element => element.id === dish.id)
+    if(object)object.amount++
+    } else {
+    const dishIdWithAmount = {
+      id: dish.id,
+      amount: 1
+    }
+    order.value.push(dishIdWithAmount);
+    }
+    const newOrder = toRaw(order.value);
+    console.log(newOrder);
+  }
+
+function removeFromOrder (dish: Dish) {
+   const object = order.value.find(element => element.id === dish.id)
+      if(object){object.amount--
+        const newOrder = toRaw(order.value);
+        console.log(newOrder);
+        if(object.amount === 0){
+          console.log("I should hide MINUS button")
+          console.log("I should delete object")
+        }
+      }
+}
+
+
 </script>
 
 <template>
@@ -68,7 +109,7 @@ const sortByCategory = computed(() => {
         :key="dish.title"
         class="bg-gray-50 px-4 py-3"
       >
-        <DishCard :dish="dish"></DishCard>
+        <DishCardMobile :dish="dish" @addToOrder="addToOrder" @removeFromOrder="removeFromOrder"></DishCardMobile>
       </div>
     </div>
 
@@ -79,7 +120,7 @@ const sortByCategory = computed(() => {
         :key="dish.title"
         class="bg-gray-50 px-4 py-3"
       >
-        <DishCard :dish="dish"></DishCard>
+        <DishCardMobile :dish="dish" @add-to-order="addToOrder" @removeFromOrder="removeFromOrder"></DishCardMobile>
       </div>
     </div>
 
@@ -90,7 +131,7 @@ const sortByCategory = computed(() => {
         :key="dish.title"
         class="bg-gray-50 px-4 py-3"
       >
-        <DishCard :dish="dish"></DishCard>
+        <DishCardMobile :dish="dish" @addToOrder="addToOrder" @removeFromOrder="removeFromOrder"></DishCardMobile>
       </div>
     </div>
     <div>
@@ -100,8 +141,13 @@ const sortByCategory = computed(() => {
         :key="dish.title"
         class="bg-gray-50 px-4 py-3"
       >
-        <DishCard :dish="dish"></DishCard>
+        <DishCardMobile :dish="dish" @addToOrder="addToOrder" @removeFromOrder="removeFromOrder"></DishCardMobile>
       </div>
     </div>
+
+    <div v-if="order" class="bg-gray-300">
+      <button>ORDER</button>
+    </div>
+
   </div>
 </template>
