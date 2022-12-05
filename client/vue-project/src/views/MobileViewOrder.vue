460611da380465f9c4b91d3669ba0599e863eed0
Ignacio Oliu <oliuignacio@gmail.com>
2022-12-05 21:07:13 +0100

    order and pay doing

--- client/vue-project/src/views/MobileViewOrder.vue
+++ client/vue-project/src/views/MobileViewOrder.vue
@@ -12,21 +12,22 @@ import LoadingSpinner from '../components/LoadingSpinner.vue';
 const orderStore = useOrderStore();
 const route = useRoute();
 const router= useRouter();
-const dishes: Ref<Dish[]> = ref([]);
-const restaurantName: Ref<string> = ref("");
+const orders: Ref<any[]> = ref([]);
+const waiter: Ref<string> = ref("");
+const restName: Ref<string> = ref("");
 const loading: Ref<boolean> = ref(true);
 const menuName: Ref<string> = ref("");
 const banner: Ref<string> = ref("");
 
-// If both params exist -> fetch restaurantDATA ---->
+// If both params exist -> fetch ticketDATA ---->
 if (route.params.restID && route.params.tableID) {
   (async () => {
-    const result = await orderAPIService.getRestaurantData(String(route.params.restID), String(route.params.tableID))
-    console.log("result:", result)
+    const result = await orderAPIService.getTicket(String(route.params.restID), String(route.params.tableID))
+    console.log("result:", result);
     if (result !== null) {
-      dishes.value = result.dishes;
-      restaurantName.value = result.name;
-      banner.value = result.banner.url
+      orders.value = result.ticket[0].orders;
+      waiter.value = result.waiter;
+      restName.value = result.restName;
     }
     loading.value = false;
   }
@@ -34,7 +35,7 @@ if (route.params.restID && route.params.tableID) {
 }
 
 async function sendOrder() {
-  // const res = await orderAPIService.newOrder(toRaw(orderStore.currentOrder) as any, orderStore.currentTotal, route.params.restID, route.params.tableID)
+  const res = await orderAPIService.newOrder(toRaw(orderStore.currentOrder) as any, orderStore.currentTotal, route.params.restID, route.params.tableID)
   //handle response
 
   router.push(route.path+'/order');
@@ -51,26 +52,30 @@ async function sendOrder() {
     <div :style="{backgroundImage: `url(${banner})`}" class= "w-full h-40 rounded-r-lg" style="background-size: cover; background-position: center;">
     
   </div>
-  <div  v-if="dishes.length">
+  <div  v-if="orders.length">
     <div class="py-2 bg-gray-200">
       <div class="flex justify-center bg-gray-200">
-        <h1 class="space-y-4 py-5 sm:py-6 text-xl">Menu - {{ restaurantName }}</h1>
-      </div>
-      <div >
-        <h2></h2>
-        <div v-for="dish in dishes" :key="dish.title" class=" flex bg-gray-200 px-4 p-0">
-          <DishCardMobile :dish="dish" />
-        </div>
+        <h1 class="space-y-4 py-5 sm:py-6 text-xl">Ticket - {{ restName }}</h1>
       </div>
       <div class="flex z-40 fixed bottom-0 w-full bg-white h-20 flex p-2 justify-center">
-        <button v-if="(orderStore.currentOrder)" class="bg-violet-700 hover:bg-violet-500 text-white font-semibold w-full rounded" @click="sendOrder">ORDER</button>
+        <button v-if="(orderStore.currentOrder)" class=" bg-violet-700 hover:bg-violet-500 text-white font-semibold w-full rounded" @click="sendOrder">ORDER</button>
       </div>
-        <!-- <ul class="flex flex-col items-center p-4">
+      Ticket:
+        <ul class="flex flex-col items-center p-4">
+          <div v-for="order in orders">
+            <div v-for="curr in order.CO">
+              <li>{{ curr.name }} , {{ curr.amount }} </li> 
+            </div>
+              {{ order.CT}} EUR
+          </div>
+        </ul>
+      New Order:
+        <ul class="flex flex-col items-center p-4">
           <div v-for="dish in orderStore.currentOrder">
             <li>{{ dish.name }}, {{ dish.amount }} u's</li>
           </div>
-        </ul> -->
-        <div class="flex items-center">TOTAL: {{ orderStore.currentTotal }}</div>
+        </ul>
+        <div class="flex items-center">new order TOTAL: {{ orderStore.currentTotal }}</div>
       </div>
   </div>
 </div>
