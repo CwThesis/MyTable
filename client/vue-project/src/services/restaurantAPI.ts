32670c1064d6e97f2a0c522822bff0e029c85831
mariarosz <roszykmaria@gmail.com>
2022-12-06 18:15:48 +0100

    feat: overview layout

--- client/vue-project/src/services/restaurantAPI.ts
+++ client/vue-project/src/services/restaurantAPI.ts
@@ -1,62 +1,107 @@
-
-const BE_URL = import.meta.env.VITE_AWS_CLOUD_LOGIC_ENDPOINT;
+const BE_URL = import.meta.env.VITE_AWS_CLOUD_LOGIC_ENDPOINT
 const restaurantAPIService = {
-
-  newRestaurant : async (restaurant: any, userID: string) => {
+  newRestaurant: async (restaurant: any, userID: string) => {
     try {
       const res = await fetch(`${BE_URL}/onboarding/${userID}`, {
-        method: "POST",
-        mode: "cors",
-        headers: {"Content-Type": "application/json", "Authorization": `${localStorage.getItem(`CognitoIdentityServiceProvider.${import.meta.env.VITE_USER_POOLS_WEB_CLIENT_ID}.${userID}.idToken`)}`}, 
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${localStorage.getItem(
+            `CognitoIdentityServiceProvider.${
+              import.meta.env.VITE_USER_POOLS_WEB_CLIENT_ID
+            }.${userID}.idToken`,
+          )}`,
+        },
         body: JSON.stringify(restaurant),
-      });
-      return await res.json();
+      })
+      return await res.json()
     } catch (err) {
-      return console.log("Error generating the new restaurant: ", err);
+      return console.log('Error generating the new restaurant: ', err)
     }
   },
 
-  getRestaurant : async (userID: string) => {
+  getRestaurant: async (userID: string) => {
     try {
       const res = await fetch(`${BE_URL}/onboarding/${userID}`, {
-        method: "GET",
-        mode: "cors",
-        headers: {"Content-Type": "application/json", "Authorization": `${localStorage.getItem(`CognitoIdentityServiceProvider.${import.meta.env.VITE_USER_POOLS_WEB_CLIENT_ID}.${userID}.idToken`)}`}, 
-      });
-      const resParsed =  await res.json();
-      return JSON.parse(resParsed.body);
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${localStorage.getItem(
+            `CognitoIdentityServiceProvider.${
+              import.meta.env.VITE_USER_POOLS_WEB_CLIENT_ID
+            }.${userID}.idToken`,
+          )}`,
+        },
+      })
+      const resParsed = await res.json()
+      return JSON.parse(resParsed.body)
     } catch (err) {
-      return console.log("Error getting the restaurant: ", err);
+      return console.log('Error getting the restaurant: ', err)
     }
   },
 
-  updateRestaurant : async (restaurant: any, userID: string) => {
+  updateRestaurant: async (restaurant: any, userID: string) => {
     try {
       const res = await fetch(`${BE_URL}/onboarding/${userID}`, {
-        method: "PUT",
-        mode: "cors",
-        headers: {"Content-Type": "application/json", "Authorization": `${localStorage.getItem(`CognitoIdentityServiceProvider.${import.meta.env.VITE_USER_POOLS_WEB_CLIENT_ID}.${userID}.idToken`)}`}, 
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${localStorage.getItem(
+            `CognitoIdentityServiceProvider.${
+              import.meta.env.VITE_USER_POOLS_WEB_CLIENT_ID
+            }.${userID}.idToken`,
+          )}`,
+        },
         body: JSON.stringify(restaurant),
-      });
-      return await res.json();
+      })
+      return await res.json()
     } catch (err) {
-      return console.log("Error updating the restaurant: ", err);
+      return console.log('Error updating the restaurant: ', err)
     }
   },
 
-  registerStripe : async (userID: string) => {
+  registerStripe: async (userID: string) => {
     try {
       const res = await fetch(`${BE_URL}/dashboard/${userID}/stripe`, {
-        method: "POST",
-        mode: "cors",
-        headers: {"Content-Type": "application/json", "Authorization": `${localStorage.getItem(`CognitoIdentityServiceProvider.${import.meta.env.VITE_USER_POOLS_WEB_CLIENT_ID}.${userID}.idToken`)}`}, 
-        body: JSON.stringify({}),
-      });
-      return await res.json();
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${localStorage.getItem(
+            `CognitoIdentityServiceProvider.${
+              import.meta.env.VITE_USER_POOLS_WEB_CLIENT_ID
+            }.${userID}.idToken`,
+          )}`,
+        },
+      })
+      return await res.json()
     } catch (err) {
-      return console.log("Couldn't save stripe registration: ", err);
+      return console.log("Couldn't save stripe registration: ", err)
+    }
+  },
+
+  stripeDashboard: async (userID: string) => {
+    try {
+      const res = await fetch(`${BE_URL}/dashboard/${userID}/stripe`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${localStorage.getItem(
+            `CognitoIdentityServiceProvider.${
+              import.meta.env.VITE_USER_POOLS_WEB_CLIENT_ID
+            }.${userID}.idToken`,
+          )}`,
+        },
+      })
+      return await res.json()
+    } catch (err) {
+      return console.log("Couldn't get the stripe dashboard url:", err)
     }
   },
 }
 
-  export default restaurantAPIService;
+export default restaurantAPIService
